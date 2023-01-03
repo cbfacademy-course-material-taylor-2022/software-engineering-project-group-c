8cbed72c3e46c38aa1225ffc91c590bbca7dcb2f
Sharrelb <sharrel.browne@outlook.com>
2023-01-03 21:54:19 +0000

    added some site image and working on home page

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import "../CSS/Site.css"
+import "../CSS/Home.css"
+
 
 const home = () => {
   return (
     <> 
-      <div className='main_contain'>
-  
-      </div>
+      <div className='mainContainer'>
+        <div className='leftContainer'> 
+          <div className='tracker'>Track Expenses</div>
+          <div className='savingPots'> Saving Pots</div>
+          <div className='Reports'> Reports</div>
+          <div className='Tips'> </div>
+        </div> 
 
+        <div className='rightContainer'>
+          <h1>FIN.ALLYY</h1>
+          <p>Welcome to Fin.Ally your financial friend</p>
+          <button> register</button>
+          <button> register</button>
+        </div>
+     </div>
     </>
   )
 }
 
-export default home
+export default home;
