47b969b3acb8fd7d6b68d0d92caec372bd803006
Sharrelb <sharrel.browne@outlook.com>
2023-01-03 21:55:01 +0000

    New images added and CSS folder

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -8,16 +8,16 @@ const home = () => {
     <> 
       <div className='mainContainer'>
         <div className='leftContainer'> 
-          <div className='tracker'>Track Expenses</div>
+          <div className='tracker'><img src="imgs/ExpenseTracker.png" alt="Puppy"/></div>
           <div className='savingPots'> Saving Pots</div>
           <div className='Reports'> Reports</div>
-          <div className='Tips'> </div>
+          <div className='Tips'> Tips </div>
         </div> 
 
         <div className='rightContainer'>
-          <h1>FIN.ALLYY</h1>
+          <h1>FIN.ALLY</h1>
           <p>Welcome to Fin.Ally your financial friend</p>
-          <button> register</button>
+          <button> login</button>
           <button> register</button>
         </div>
      </div>
