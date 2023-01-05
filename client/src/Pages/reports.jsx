5e26fc14a8d24eebc6e86cdf5fd6ae96d13fe607
Tlorriene <tlorriene@icloud.com>
2023-01-05 17:48:05 +0000

    initial components for reports page

---
+++ client/src/Pages/reports.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../components/logo";
+import sideMenu from "../components/sideMenu";
+
+
+function Heading (props){
+    return <h1>{props.name}'s Reports</h1>
+}
+
